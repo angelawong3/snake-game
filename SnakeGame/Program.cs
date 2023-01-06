c45084a3c19e3d8de335833f931153fc75c3cdda
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2023-01-06 16:55:43 +0000

    set height and width for the game

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
 
+namespace SnakeGame
+{
+    class Snake
+    {
+        int Height = 30;
+        int Width = 50;
+
+        public void WriteBoard()
+        {
+            Console.Clear();
+            for(int i = 1; i<=(Width+2); i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write("-");
+            }
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, (Height+2));
+                Console.Write("-");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write("-");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition((Width+2), i);
+                Console.Write("-");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Snake snake = new Snake();
+            snake.WriteBoard();
+            Console.ReadKey();
+        }
+    }
+}
