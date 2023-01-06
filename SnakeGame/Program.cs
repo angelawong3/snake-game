1a38b70469378036cfbb0b49de79370fd226968a
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2023-01-06 17:28:43 +0000

    snake game done, tested

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -5,8 +5,30 @@ namespace SnakeGame
 {
     class Snake
     {
-        int Height = 30;
-        int Width = 50;
+        int Height = 20;
+        int Width = 30;
+
+        int[] X = new int[50];
+        int[] Y = new int[50];
+
+        int pointX;
+        int pointY;
+
+        int parts = 3;
+
+        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+        char key = 'W';
+
+        Random round = new Random();
+
+        Snake()
+        {
+            X[0] = 5;
+            Y[0] = 5;
+            Console.CursorVisible = false;
+            pointX = round.Next(2, (Width - 2));
+            pointY = round.Next(2, (Height - 2));
+        }
 
         public void WriteBoard()
         {
@@ -24,18 +46,78 @@ namespace SnakeGame
             for (int i = 1; i <= (Height + 1); i++)
             {
                 Console.SetCursorPosition(1, i);
-                Console.Write("-");
+                Console.Write("|");
             }
             for (int i = 1; i <= (Height + 1); i++)
             {
                 Console.SetCursorPosition((Width+2), i);
-                Console.Write("-");
+                Console.Write("|");
             }
         }
+
+        public void Input()
+        {
+            if (Console.KeyAvailable)
+            {
+                keyInfo = Console.ReadKey(true);
+                key = keyInfo.KeyChar;
+            }
+        }
+        public void WritePoint(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("*");
+        }
+
+        public void Logic()
+        {
+            if (X[0] == pointX)
+            {
+                if (Y[0] == pointY)
+                {
+                    parts++;
+                    pointX = round.Next(2, (Width - 2));
+                    pointY = round.Next(2, (Height - 2));
+
+                }
+            }
+            for(int i = parts; i > 1; i--)
+            {
+                X[i - 1] = X[i - 2];
+                Y[i - 1] = Y[i - 2];
+            }
+            switch(key)
+            {
+                case 'w':
+                    Y[0]--;
+                    break;
+                case 's':
+                    Y[0]++;
+                    break;
+                case 'd':
+                    X[0]++;
+                    break;
+                case 'a':
+                    X[0]--;
+                    break;
+            }
+            for(int i = 0; i <= (parts-1); i++)
+            {
+                WritePoint(X[i], Y[i]);
+                WritePoint(pointX, pointY);
+            }
+            Thread.Sleep(100);
+        }
+
         static void Main(string[] args)
         {
             Snake snake = new Snake();
-            snake.WriteBoard();
+            while (true)
+            {
+                snake.WriteBoard();
+                snake.Input();
+                snake.Logic();
+            }
             Console.ReadKey();
         }
     }
